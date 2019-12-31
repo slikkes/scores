6ed2a89a12a3d75733ccddebef0b78a52ed523dc
slikkes <slikkistvan@gmail.com>
2019-12-31 10:07:40 +0100

    test

--- src/Api.js
+++ src/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:8081`
+    //baseURL: `http://localhost:8081`
+    baseURL: `http://ec2-18-222-153-215.us-east-2.compute.amazonaws.com:8081`
   })
 }
