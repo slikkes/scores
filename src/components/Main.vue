6c70a9bb18f0909a6fbe64b8119b82d56c438342
slikkes <slikkistvan@gmail.com>
2019-12-31 16:59:04 +0100

    cosmetics

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,61 +1,82 @@
 <template>
-  <section class="is-fullheight">
-    <div class="columns" style="padding:20px">
-      <div v-for="team in sortedTeams" :class="`column hero is-${team.color}`" style="margin: 10px">
-        <div class="columns">
-          <div class="column">
+  <div>
+    <section class="is-fullheight">
+      <div class="columns is-multiline is-3 " style="padding:20px">
+        <div v-for="team in sortedTeams" :class="`column is-4  hero is-${team.color}`">
+          <div>
             <h1 class="title">
-              {{team.name}}
-            </h1>
+            {{team.name}}
+          </h1>
           </div>
-          <div class="column" style="text-align:right">
-            <h1 class="title">
+          <div>
+            <h1 class="title" style="font-size:128px">
               {{team.score}}
-            </h1>
-          </div>
+            </h1></div>
         </div>
-
       </div>
-    </div>
-  </section>
+    </section>
+    <footer class="footer" style="position:fixed; bottom:0; width:100vw;">
+      <div class="content has-text-centered">
+        <h1 class="title" style="font-size:64px">
+          {{timeRemaining}}
+        </h1>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
 import Api from "../Api";
-require('../../node_modules/bulma/css/bulma.css');
+
+require ('../../node_modules/bulma/css/bulma.css');
 import pusher from 'pusher-js';
 
 export default {
   name: "Main",
   data() {
     return {
-      teams: []
+      teams: [],
+      timeRemaining: ""
     }
   },
   created() {
-    this.fetchData();
-    let pusher = new Pusher('befc1e969f052ccc38af', {
+    this.fetchData ();
+    this.countDownTimer();
+    let pusher = new Pusher ('befc1e969f052ccc38af', {
       cluster: 'eu',
       //   encrypted: true
     });
-    console.log('asdf')
-    let channel = pusher.subscribe('team');
-    channel.bind('updated', () => {
-      this.fetchData()
+    console.log ('asdf')
+    let channel = pusher.subscribe ('team');
+    channel.bind ('updated', () => {
+      this.fetchData ()
     });
   },
-  computed:{
-    sortedTeams(){
-      return this.teams.sort((a,b)=> a.score > b.score ? -1 : 1)
+  computed: {
+    sortedTeams() {
+      return this.teams.sort ((a, b) => a.score > b.score ? -1 : 1)
     },
   },
-  methods:{
+  methods: {
     fetchData() {
-      Api().get ("/teams")
+      Api ().get ("/teams")
         .then (response => {
           this.teams = response.data
         })
     },
+    countDownTimer() {
+        setTimeout(() => {
+          let hour = 24 - new Date().getHours();
+          let min = 60 - new Date().getMinutes();
+          let sec = 60 - new Date().getSeconds();
+
+          hour = hour < 10 ? `0${hour}` : hour;
+          min = min < 10 ? `0${min}` : min;
+          sec = sec < 10 ? `0${sec}` : sec;
+          this.timeRemaining = `${hour}:${min}:${sec}`;
+          this.countDownTimer()
+        }, 1000)
+    }
   }
 }
 </script>
